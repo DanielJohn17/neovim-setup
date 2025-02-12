af116f6735db4bbcdc1c6a0e518a147c45532095
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-12 23:10:51 +0300

    Added oil.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -51,6 +51,19 @@ local plugins = {
 
   -- copilot
   'github/copilot.vim',
+
+  --oil nvim
+  {
+  'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  }
 }
 
 local opts = {}
