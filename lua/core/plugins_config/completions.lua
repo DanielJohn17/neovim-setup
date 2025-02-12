bf5c083a6028e10bbebcd1cd892f6b660ace1515
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-12 20:18:41 +0300

    lsp configs for other languages

--- lua/core/plugins_config/completions.lua
+++ lua/core/plugins_config/completions.lua
@@ -3,12 +3,16 @@ local cmp = require("cmp")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
+  completion = {
+    autocomplete = { require("cmp.types").cmp.TriggerEvent.TextChanged },
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.complete(),
   }),
   snippet = {
     expand = function(args)
