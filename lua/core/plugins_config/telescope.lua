3e46f857a45e3c63b9e7e00dc39dc83bd8f0384f
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-15 23:30:58 +0300

    telescope buffer and treesitter, also can find dotfiles

--- lua/core/plugins_config/telescope.lua
+++ lua/core/plugins_config/telescope.lua
@@ -1,30 +1,36 @@
 local status, builtin = pcall(require, "telescope.builtin")
 if not status then
-  print("Telescope is not installed properly!")
-  return
+	print("Telescope is not installed properly!")
+	return
 end
 
 vim.keymap.set("n", "<c-p>", builtin.find_files, {})
 vim.keymap.set("n", "<Space><Space>", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<Space>fb", builtin.buffers, {})
+vim.keymap.set("n", "<Space>ft", builtin.treesitter, {})
 
 require("telescope").setup({
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-    }
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown({}),
-    },
-  },
+	defaults = {
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		file_ignore_patterns = {
+			"node_modules",
+			".git",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
 })
 
 require("telescope").load_extension("ui-select")
