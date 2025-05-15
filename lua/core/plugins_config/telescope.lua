cd898c41b4552bede56be388be38b53f20fa13eb
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-15 21:16:39 +0300

    telescope can now find hidden files

--- lua/core/plugins_config/telescope.lua
+++ lua/core/plugins_config/telescope.lua
@@ -1,7 +1,7 @@
 local status, builtin = pcall(require, "telescope.builtin")
 if not status then
-	print("Telescope is not installed properly!")
-	return
+  print("Telescope is not installed properly!")
+  return
 end
 
 vim.keymap.set("n", "<c-p>", builtin.find_files, {})
@@ -10,16 +10,21 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
 
 require("telescope").setup({
-	defaults = {
-		prompt_prefix = " ",
-		selection_caret = " ",
-		path_display = { "smart" },
-	},
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown({}),
-		},
-	},
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    }
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({}),
+    },
+  },
 })
 
 require("telescope").load_extension("ui-select")
