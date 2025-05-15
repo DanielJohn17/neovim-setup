1cad0294bd3fc35c2cc69a35417732f3d32bd25e
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-15 23:29:54 +0300

    added more treesitter languages to parse

--- lua/core/plugins_config/treesitter.lua
+++ lua/core/plugins_config/treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "javascript", "go", "php", "html", "css", "csv", "sql" },
+  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "javascript", "go", "php", "html", "css", "csv", "sql", "bash", "json", "yaml", "toml", "regex", "rust", "java", "php", "dockerfile", "typescript" },
 
   sync_install = false,
 
