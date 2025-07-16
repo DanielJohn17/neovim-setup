ea609df3dec3664b63aed9a306d998b95fbc00ff
DanielJohn17 <danielyohannes38@gmail.com>
2025-07-16 15:03:52 +0300

    pg_format

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -10,6 +10,9 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.golines,
+				null_ls.builtins.formatting.pg_format.with({
+					extra_args = { "--keyword-case", "2", "--spaces", "2" },
+				}),
 			},
 		})
 
