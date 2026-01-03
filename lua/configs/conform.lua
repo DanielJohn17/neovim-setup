ec064af9c80ef4c5bebcca951e034a72a9a07579
DanielJohn17 <danielyohannes38@gmail.com>
2026-01-03 22:15:58 +0300

    Add: add prettier formatter for web files

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,29 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+
+    -- Web
     css = { "prettier" },
     html = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+
+    -- Go
     go = { "golines" },
+
+    -- templ
+    templ = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
